5f94cf349685b4e5475c645162f6666295c17af5
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-04-24 23:51:12 +0800

    adeed layout to another page

--- src/pages/projects/{mdx.slug}.js
+++ src/pages/projects/{mdx.slug}.js
@@ -6,14 +6,16 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 export default function Project({ data }) {
   const image = getImage(data.mdx.frontmatter.headerImage);
   return (
-    <div className="max-w-screen-sm mx-auto">
-      <div className="flex flex-col items-center max-w-md mx-auto mb-8 text-center">
-        <GatsbyImage image={image} alt="alt" />
-        <h1 className="text-4xl font-bold">{data.mdx.frontmatter.title}</h1>
-        <p className="text-lg">{data.mdx.frontmatter.summary}</p>
+    <Layout>
+      <div className="max-w-screen-sm mx-auto">
+        <div className="flex flex-col items-center max-w-md mx-auto mb-8 text-center">
+          <GatsbyImage image={image} alt="alt" />
+          <h1 className="text-4xl font-bold">{data.mdx.frontmatter.title}</h1>
+          <p className="text-lg">{data.mdx.frontmatter.summary}</p>
+        </div>
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </div>
-      <MDXRenderer>{data.mdx.body}</MDXRenderer>
-    </div>
+    </Layout>
   );
 }
 
